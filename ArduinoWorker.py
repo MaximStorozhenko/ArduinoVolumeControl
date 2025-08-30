a2c131d16ffc21c25d29707c2679cfb5706eb254
Maxim Storozhenko <maxim.storojenko@gmail.com>
2025-08-30 18:52:24 +0300

    Conflict

---
+++ ArduinoWorker.py
@@ -0,0 +1,108 @@
+import time
+import threading
+
+import serial
+import serial.tools.list_ports
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from comtypes import CLSCTX_ALL
+
+class ArduinoWorker:
+    def __init__(self, port="COM14", baudrate=9600):
+        self.port = port
+        self.baudrate = baudrate
+        self.arduino = None
+        self.running = threading.Event()
+        self.thread = None
+
+        # Получение громкости звука из системы
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        self.volume = interface.QueryInterface(IAudioEndpointVolume)
+        ############
+
+    #Установка громкости
+    def SetVolume(self, percent: int):
+        self.volume.SetMasterVolumeLevelScalar(percent/100, None)
+
+    #Получение громкости
+    def GetVolume(self) -> int:
+        level = self.volume.GetMasterVolumeLevelScalar()
+        return int(round(level * 100))
+
+    #Выключение звука
+    def ToggleMute(self):
+        self.volume.SetMute(not self.volume.GetMute(), None)
+
+    #Получение mute звука
+    #return bool
+    def GetMute(self) -> bool:
+        return self.volume.GetMute()
+
+    #Поиск ардуино
+    def ConnectArduino(self):
+        try:
+            return serial.Serial(self.port, self.baudrate, timeout=1)
+        except serial.SerialException:
+            return None
+
+    #Отключение ардуино
+    def ArduinoClose(self):
+        if self.arduino:
+            try:
+                self.arduino.close()
+            except:
+                pass
+            self.arduino = None
+
+    def WorkerLoop(self):
+        try:
+            while self.running.is_set():
+                if self.arduino is None or not self.arduino.is_open:
+                    print("\nОжидание Arduino...")
+                    while self.running.is_set() and self.arduino is None:
+                        self.arduino = self.ConnectArduino()
+                        time.sleep(2)
+
+                    if self.arduino: print("Arduino подключено!")
+
+                try:
+                    if self.arduino and self.arduino.in_waiting:
+                        cmd = self.arduino.readline().decode().strip()
+                        vol = self.GetVolume()
+                        if cmd == 'UP':
+                            vol = min(100, vol + 2)
+                            self.SetVolume(vol)
+                        elif cmd == "DOWN":
+                            vol = max(0, vol - 2)
+                            self.SetVolume(vol)
+                        elif cmd == "MUTE":
+                            self.ToggleMute()
+
+                    if self.arduino:
+                        if self.GetMute():
+                            self.arduino.write(b"MUTE\n")
+                        else:
+                            self.arduino.write(f"{self.GetVolume()}\n".encode())
+                    time.sleep(0.1)
+
+                except (serial.SerialException, OSError):
+                    print("Потеряно соединение с Arduino.")
+                    self.ArduinoClose()
+        finally:
+            self.ArduinoClose()
+            print("ArduinoWorker остановлен.")
+
+    # Управление потоко
+    def start(self):
+        if self.thread and self.thread.is_alive():
+            return  # уже запущен
+        print("Запуск ArduinoWorker...")
+        self.running.set()
+        self.thread = threading.Thread(target=self.WorkerLoop, daemon=True)
+        self.thread.start()
+
+    def stop(self):
+        print("Остановка ArduinoWorker...")
+        self.running.clear()
+        if self.thread:
+            self.thread.join()
--- main.py
+++ main.py
@@ -1,89 +1,19 @@
-import math
-import time
-import serial
-import serial.tools.list_ports
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-from comtypes import CLSCTX_ALL
+from ArduinoWorker import ArduinoWorker
 
-import sys
-from PyQt5.QtWidgets import QApplication
-from TrayApp import TrayApp
+from TrayApp import TrayIcon
 
-BAUDRATE = 9600
-arduino = None
+def main():
+    worker = ArduinoWorker("COM14")
+    worker.start()
 
-# Получение громкости звука
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = interface.QueryInterface(IAudioEndpointVolume)
-############
+    def ExitProgram():
+        print("Выход из программы...")
+        worker.stop()
 
-#Установка громкости
-def SetVolume(percent):
-    volume.SetMasterVolumeLevelScalar(percent/100, None)
+    tray = TrayIcon(ExitProgram)
+    tray.Run()
 
-#Получение громкости
-def GetVolume():
-    level = volume.GetMasterVolumeLevelScalar()
-    return int(round(level * 100))
+    print("Программа завершена.")
 
-#Выключение звука
-def ToggleMute():
-    volume.SetMute(not volume.GetMute(), None)
-
-#Получение mute звука
-#return bool
-def GetMute():
-    return volume.GetMute()
-
-#Поиск ардуино
-def ConnectArduino(port, baudrate):
-    try:
-        return serial.Serial(port, baudrate, timeout=1)
-    except serial.SerialException:
-        return None
-
-#Отключение ардуино
-def ArduinoClose():
-    if arduino:
-        try:
-            arduino.close()
-        except:
-            pass
-
-try:
-    while True:
-        if arduino is None or not arduino.is_open:
-            print("\nОжидание Arduino...")
-            while arduino is None:
-                arduino = ConnectArduino("COM14", BAUDRATE)
-                time.sleep(2)
-            print("Arduino подключено!")
-
-        try:
-            if arduino.in_waiting:
-                cmd = arduino.readline().decode().strip()
-                vol = GetVolume()
-                if cmd == 'UP':
-                    vol = min(100, vol + 2)
-                    SetVolume(vol)
-                elif cmd == "DOWN":
-                    vol = max(0, vol - 2)
-                    SetVolume(vol)
-                elif cmd == "MUTE":
-                    ToggleMute()
-
-            if GetMute():
-                arduino.write(b"MUTE\n")
-            else:
-                arduino.write(f"{GetVolume()}\n".encode())
-            time.sleep(0.1)
-
-        except (serial.SerialException, OSError):
-            print("Потеряно соединение с Arduino.")
-            ArduinoClose()
-            arduino = None  # снова ждать подключения
-
-except KeyboardInterrupt:
-    ArduinoClose()
-    print("\nThe program was stopped by the user.")
+if __name__ == "__main__":
+    main()
